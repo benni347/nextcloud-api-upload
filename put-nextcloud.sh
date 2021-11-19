56237d4dd025f70ca8ac5a317a2c52db5ecf948e
benni347 <cdrc+gitkraken@skwar.me>
2021-11-19 16:41:58 +0100

    Make use of remote_php variable and remove commented out code.

--- put-nextcloud.sh
+++ put-nextcloud.sh
@@ -2,13 +2,6 @@
 
 . "settings.cfg"
 
-#curl=$(curl -s -u "$user:$pw" "https://nc.systeme.guru/remote.php/dav/files/$user/$folder/" -T "./$folder/")
-#echo $curl
-
-#DATA=$(find drive -type f -exec $curl)
-
-find drive -type f -exec curl -s -u "$user:$pw" "https://nc.systeme.guru/remote.php/dav/files/$user/$folder/" -T '{}' ';'
-
-#echo $DATA
+find "$folder" -type f -exec curl -s -u "$user:$pw" "$remote_php/dav/files/$user/$folder/" -T '{}' ';'
 
 exit $?
